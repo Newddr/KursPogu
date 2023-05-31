48f1d5c5b657c2ee6b842310f947c6e8444f4ed3
Newddr <fedtur62@yandex.ru>
2023-05-31 18:33:52 +0800

    Убрал заказы у повара

--- KursPogud/Program.cs
+++ KursPogud/Program.cs
@@ -18,7 +18,7 @@ namespace KursPogud
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form4());
+            Application.Run(new Form2());
         }
     }
 }
